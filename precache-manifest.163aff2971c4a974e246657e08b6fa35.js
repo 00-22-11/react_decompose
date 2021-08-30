51f739693e53fe32e92f29fa634f496752177dd1
Artem <violence044@gmail.com>
2021-08-30 20:25:58 +0300

    Updates

---
+++ precache-manifest.163aff2971c4a974e246657e08b6fa35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522c8b5ca81bf36f9b486ba9c1b2e6f9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "17f5c2ebdda27aaa4bcd",
+    "url": "./static/css/main.a86ecf9a.chunk.css"
+  },
+  {
+    "revision": "8fa8843e8c88d99aef56",
+    "url": "./static/js/2.24585e7e.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.24585e7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17f5c2ebdda27aaa4bcd",
+    "url": "./static/js/main.7fcaead0.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
